b0dd635f81df9d89bade1e9646db39229252d9f2
Patrick Justice <patrickjustice@yahoo.com>
2024-04-25 16:23:08 -0400

    added purple logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,35 +3,37 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import whiteLogo from '../image/White Emblem.PNG'
+import whiteLogo from "../image/White Emblem.PNG";
+import purplelogo from '../image/Detached logo .PNG';
 
 // import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function NavScrollExample() {
+function Header() {
   return (
-    <Navbar expand="lg" bg="dark" variant="dark" >
-      <Container fluid>
-        <Navbar.Brand href="#">
-        <img
-            src={whiteLogo}
-            height="30"
-            className="d-inline-block align-top"
-            alt="Band Logo"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Music</Nav.Link>
-            <Nav.Link href="#action2">Merch</Nav.Link>
-            <Nav.Link href="#action2">Socials</Nav.Link>
+    <div>
+      <Navbar expand="lg" bg="dark" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="#">
+            <img
+              src={whiteLogo}
+              height="30"
+              className="d-inline-block align-top"
+              alt="Band Logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Music</Nav.Link>
+              <Nav.Link href="#action2">Merch</Nav.Link>
+              <Nav.Link href="#action2">Socials</Nav.Link>
 
-            {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
+              {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action4">
                 Another action
@@ -41,20 +43,30 @@ function NavScrollExample() {
                 Something else here
               </NavDropdown.Item>
             </NavDropdown> */}
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="text-center mt-3">
+        <img
+          src={purplelogo}
+          height="300"
+          className="d-inline-block align-top"
+          alt="Band Logo"
+        />
+      </div>
+    </div>
   );
 }
 
-export default NavScrollExample;
+export default Header;
